f9cfbb002e043ab62ed71887e1f9ce4611a0e69c
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-22 09:20:44 -0700

    Add model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :studio
+
+  validates_presence_of :title
+  validates_presence_of :creation_year
+  validates_presence_of :genre
 end
--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,6 @@
 class Studio < ApplicationRecord
   has_many :movies
+
+  validates_presence_of :name
+  validates_presence_of :location
 end
